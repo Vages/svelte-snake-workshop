6f7a76893133d1e24bc25c7c07c5b4721dc133a1
Eirik Vageskar <eirikvageskar@gmail.com>
2021-06-20 22:29:54 +0200

    :wrench: Add back some rules from task-5.3-end

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,17 @@ module.exports = {
     {
       files: ["*.svelte"],
       processor: "svelte3/svelte3",
+      rules: {
+        "prefer-const": "off",
+      },
+      settings: {
+        "svelte3/ignore-warnings": (output) => {
+          if (output.code === "css-unused-selector") {
+            return true;
+          }
+          return false;
+        },
+      },
     },
   ],
   rules: { "no-use-before-define": ["error", "nofunc"] },
