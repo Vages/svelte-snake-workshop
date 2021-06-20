06261b0d1a71da7ddf33f48c710f8b751e6c23ea
Eirik Vageskar <eirikvageskar@gmail.com>
2021-06-20 23:24:54 +0200

    :recycle: sort files

--- src/App.svelte
+++ src/App.svelte
@@ -21,6 +21,37 @@
   };
 </script>
 
+<div use:cssVars={styleVars} class="main-content min-width">
+  <div class="score">{score}</div>
+
+  <div
+    class="board"
+    style="width: {BOARD_DIMENSIONS.x *
+      CELL_SIZE}px; height: {BOARD_DIMENSIONS.y * CELL_SIZE}px"
+  >
+    <div class="body-part" />
+
+    <div
+      style="left: {apple.x * CELL_SIZE}px; top: {apple.y * CELL_SIZE}px"
+      class="apple"
+    />
+  </div>
+
+  <div class="signature">
+    <p>
+      Made with
+      <a href="https://svelte.dev/">
+        <img alt="Svelte logo" src="/svelte.png" />
+        Svelte
+      </a>
+      in the
+      <a href="https://github.com/Vages/svelte-snake-workshop">
+        Svelte Snake Workshop
+      </a>
+    </p>
+  </div>
+</div>
+
 <style>
   .modal-container {
     position: absolute;
@@ -118,34 +149,3 @@
       calc(-1 * var(--cell-size)) 0;
   }
 </style>
-
-<div use:cssVars={styleVars} class="main-content min-width">
-  <div class="score">{score}</div>
-
-  <div
-    class="board"
-    style="width: {BOARD_DIMENSIONS.x *
-      CELL_SIZE}px; height: {BOARD_DIMENSIONS.y * CELL_SIZE}px"
-  >
-    <div class="body-part" />
-
-    <div
-      style="left: {apple.x * CELL_SIZE}px; top: {apple.y * CELL_SIZE}px"
-      class="apple"
-    />
-  </div>
-
-  <div class="signature">
-    <p>
-      Made with
-      <a href="https://svelte.dev/">
-        <img alt="Svelte logo" src="/svelte.png" />
-        Svelte
-      </a>
-      in the
-      <a href="https://github.com/Vages/svelte-snake-workshop">
-        Svelte Snake Workshop
-      </a>
-    </p>
-  </div>
-</div>
