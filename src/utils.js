d54e505d9dd5e24564ec2183dad8970fde08fa78
Eirik Vågeskar <playground.gate@protonmail.com>
2021-11-29 13:56:02 +0100

    :bulb: Add docstrings to all functions in utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+// TODO 2021-11-29 (Eirik V.): Rename DIRECTION
+/**
+ * An enumeration used to describe the different directions on the board.
+ * The directions are as on a most maps, with north pointing up and east to the right.
+ */
 export const DIRECTION_NAMES = Object.freeze({
   NORTH: "NORTH",
   EAST: "EAST",
@@ -5,11 +10,13 @@ export const DIRECTION_NAMES = Object.freeze({
   WEST: "WEST",
 });
 
+/** An array containing tuples of opposite directions */
 const OPPOSITE_DIRECTIONS = [
   [DIRECTION_NAMES.NORTH, DIRECTION_NAMES.SOUTH],
   [DIRECTION_NAMES.EAST, DIRECTION_NAMES.WEST],
 ];
 
+/** A map converting each DIRECTION to a coordinate */
 export const DIRECTION_VECTORS = Object.freeze({
   [DIRECTION_NAMES.NORTH]: { x: 0, y: -1 },
   [DIRECTION_NAMES.SOUTH]: { x: 0, y: 1 },
@@ -17,14 +24,17 @@ export const DIRECTION_VECTORS = Object.freeze({
   [DIRECTION_NAMES.WEST]: { x: -1, y: 0 },
 });
 
+/** Adds the two coordinates' x and y values together */
 export function add(coordinateA, coordinateB) {
   return { x: coordinateA.x + coordinateB.x, y: coordinateA.y + coordinateB.y };
 }
 
+/** Tells whether the two coordinates are the same */
 export function isEqual(coordinateA, coordinateB) {
   return coordinateA.x === coordinateB.x && coordinateA.y === coordinateB.y;
 }
 
+/** Tells whether the given coordinate is inside the board */
 export function isInsideBoard(boardDimensions, coordinate) {
   return (
     coordinate.x >= 0 &&
@@ -34,6 +44,7 @@ export function isInsideBoard(boardDimensions, coordinate) {
   );
 }
 
+/** Moves the snake one space in the given direction */
 export function getNextSnake(snake, direction, shouldGrow = false) {
   const headCoordinate = snake[0];
   const nextHead = add(headCoordinate, direction);
@@ -44,6 +55,8 @@ export function getNextSnake(snake, direction, shouldGrow = false) {
     : withAddedHead.slice(0, withAddedHead.length - 1);
 }
 
+// TODO 2021-11-29 (Eirik V.): Rename to drawRandomOpenSpace
+/** Draws a random space from the available spaces */
 export function getNewApplePosition(boardDimensions, snake) {
   const boardSpaces = [...Array(boardDimensions.x).keys()].flatMap((x) =>
     [...Array(boardDimensions.y).keys()].map((y) => ({ x, y }))
@@ -55,11 +68,13 @@ export function getNewApplePosition(boardDimensions, snake) {
   return openSpaces[Math.floor(Math.random() * openSpaces.length)];
 }
 
+/** Tells whether a snake's head is inside some other part of its body */
 export function isSnakeEatingItself(snake) {
   const headPosition = snake[0];
   return snake.slice(1).some((snakeSpace) => isEqual(snakeSpace, headPosition));
 }
 
+/** Tells whether two directions are perpendicular (i.e. meet at a 90 degree angle) */
 function arePerpendicular(directionA, directionB) {
   const directionsAreSameOrOpposite = OPPOSITE_DIRECTIONS.some(
     (pairOfOpposites) =>
@@ -69,6 +84,8 @@ function arePerpendicular(directionA, directionB) {
   return !directionsAreSameOrOpposite;
 }
 
+// TODO 2021-11-29 (Eirik V.): Rename to convertEventKeyToDirection
+/** Turns key names into directions */
 export function getNewDirectionFromEventKey(key) {
   // Also contains a secret Dvorak mode
   switch (key) {
@@ -92,6 +109,12 @@ export function getNewDirectionFromEventKey(key) {
   }
 }
 
+// TODO 2021-11-29 (Eirik V.): Rename to shiftQueueUntilNextPerpendicularMove or something like it
+/**
+ * Finds the first perpendicular direction in queue
+ * (i.e. ignores any u-turn and continue-forward key presses)
+ * and returns that direction as well as the remaining queue.
+ */
 export function getNextHeadDirectionAndQueue(queue, currentDirection) {
   const foundIndex = queue.findIndex((direction) =>
     arePerpendicular(currentDirection, direction)
