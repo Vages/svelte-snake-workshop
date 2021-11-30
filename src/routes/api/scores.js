b5780387ae918d5fec1d127b195a28ec7b684b3c
Eirik Vågeskar <playground.gate@protonmail.com>
2021-11-30 13:26:59 +0100

    :bug: Sort scores before returning them

--- src/routes/api/scores.js
+++ src/routes/api/scores.js
@@ -1,10 +1,11 @@
 let scores = [
-  { name: "Alice", score: 10 },
   { name: "Bob", score: 42 },
+  { name: "Alice", score: 10 },
 ];
 
 export async function post(request) {
   scores.push(JSON.parse(request.body));
+  scores.sort((a, b) => b.score - a.score);
   return {
     status: 200,
     body: scores,
