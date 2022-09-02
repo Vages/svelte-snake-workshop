e915e34ec3417b1146839b10de9fdf1e821a9a1f
Eirik Vågeskar <eirik.vageskar@protonmail.com>
2022-09-02 21:50:26 +0200

    Rewrite front page in Svelte instead of mdsvex
    Remove mdsvex loader.
    Remove all mdsvex related configuration.
    
    Closes #18

---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<script>
+	const tasks = [
+		"1.1",
+		"1.2",
+		"1.3",
+		"2.1",
+		"2.2",
+		"3.1",
+		"3.2",
+		"3.3",
+		"3.4",
+		"3.5",
+		"3.6",
+		"4.1",
+		"4.2",
+		"4.3",
+		"4.4",
+		"5.1",
+		"5.2",
+		"5.3",
+	];
+</script>
+
+<h1>Welcome to the Svelte workshop!</h1>
+
+<p>Open the file TASKS.md to get going.</p>
+
+<table>
+	<thead>
+		<tr>
+			<th>Task</th>
+			<th>Working area</th>
+			<th>Suggested solution</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each tasks as task}
+			<tr>
+				<td>{task}</td>
+				<td><a href="/task-{task}/0-working-area">Working area</a></td>
+				<td><a href="/task-{task}/1-solution">Suggested solution</a></td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	table {
+		border: 2px solid black;
+	}
+
+	td,
+	th {
+		border: 2px solid black;
+		padding: 0.75rem;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
 import adapter from "@sveltejs/adapter-auto";
-import { mdsvex } from "mdsvex";
-import sveltePreprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 	},
-
-	// Add support for mdsvex
-	extensions: [".svelte", ".svx", ".md"],
-	preprocess: [mdsvex(), sveltePreprocess()],
 };
 
 export default config;
